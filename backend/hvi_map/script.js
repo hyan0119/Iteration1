d85a8b6ae7021ab8a35cca084ece5b3d487a46b9
Rentaro Yamamoto <49111485+ren170904@users.noreply.github.com>
2024-04-07 23:22:44 +1000

    final

--- backend/hvi_map/script.js
+++ backend/hvi_map/script.js
@@ -48,37 +48,37 @@ let drawMap = () => {
         // .style("visibility", "visible")
 
         if (d.properties.HVI == 0) {
-            tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is not vulnerable to Urban Heat Island.")
+            tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is not any warmer the countryside, so you would not notice any difference." + "<br>" + "It is as if this suburb is part of the natural landscape, with plenty of trees and parks that help keep it cool.")
             .style("left", (event.pageX + 10) + "px")
             .style("top", (event.pageY - 10) + "px")
             .style("display", "block")
             .style("visibility", "visible")
         } else if (d.properties.HVI == 1) {
-            tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is slightly vulnerable to Urban Heat Island.")
+            tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "You might feel that this suburb is a bit warmer here than in the countryside, but it is barely noticeable. This suburb might have some green spaces and buildings that do not get too hot during hot days, so it does not make much of a difference to your day or your electricity bill.")
             .style("left", (event.pageX + 10) + "px")
             .style("top", (event.pageY - 10) + "px")
             .style("display", "block")
             .style("visibility", "visible")
         } else if (d.properties.HVI == 2) {
-            tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" +  "This suburb is moderately vulnerable to Urban Heat Island.")
+            tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" +  "This suburb is warmer than the countryside. You might start turning on the air conditioning more often, and it could feel uncomfortably hot outside on some days. You might want to consider growing plants in your balcony, rooftop, or in rooms inside your house to add natural shades and help cooling down your area.")
             .style("left", (event.pageX + 10) + "px")
             .style("top", (event.pageY - 10) + "px")
             .style("display", "block")
             .style("visibility", "visible")
         } else if (d.properties.HVI == 3) {
-            tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is highly vulnerable to Urban Heat Island.")
+            tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb experiences significantly higher temperatures than the countryside, especially during hot days, encouraging indoor stays with air conditioning and potentially impacting health. Installing cool or green roofs to reflect sunlight and heat, along with growing plants on balconies, rooftops, or indoors, can offer natural shade and cool the area.")
             .style("left", (event.pageX + 10) + "px")
             .style("top", (event.pageY - 10) + "px")
             .style("display", "block")
             .style("visibility", "visible")
         } else if (d.properties.HVI == 4) {
-            tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is extremely vulnerable to Urban Heat Island.")
+            tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb, hotter than the countryside, impacts comfort, increases electricity bills, and poses health risks in summer. To combat this, it needs more parks, green roofs, and cooling centers. Installing cool roofs and growing plants in your home can provide shade and cool your area, reducing the summer heat's adverse effects.")
             .style("left", (event.pageX + 10) + "px")
             .style("top", (event.pageY - 10) + "px")
             .style("display", "block")
             .style("visibility", "visible")
         } else if (d.properties.HVI == 5) { 
-            tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is severely vulnerable to Urban Heat Island.")
+            tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb's extreme heat, beyond countryside levels, brings discomfort and health dangers. Mitigating it demands significant urban upgrades and cooling solutions. Installing cool or green roofs and growing plants indoors can shade and cool the area, easing summer and heatwave effects.")
             .style("left", (event.pageX + 10) + "px")
             .style("top", (event.pageY - 10) + "px")
             .style("display", "block")
@@ -117,7 +117,7 @@ let drawMap = () => {
             });
         }
         // 新しいパスの色を変更
-        d3.select(this).style("fill", "pink");
+        d3.select(this).style("fill", "#33333e");
         // 選択されたパスを更新
         selectedPath = d3.select(this);
         
@@ -133,7 +133,6 @@ let drawMap = () => {
     }).on("mouseup", function() {
     isClicking = false;
     });
-
 
 
 }
