782b59939b92aeb70bbe0c3efbeafa3c37d5b94f
Rentaro Yamamoto <49111485+ren170904@users.noreply.github.com>
2024-04-10 22:21:57 +1000

    Update UhiMap.vue

--- frontend/src/views/UhiMap.vue
+++ frontend/src/views/UhiMap.vue
@@ -161,64 +161,64 @@ export default {
                 })
                 .style("stroke", "white")
                 .style("stroke-width", 0.5)
-                .on("mouseover", function (event, d) {
-                    // マウスオーバー時の処理
-                    // tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI)
-                    // .style("left", (event.pageX + 10) + "px")
-                    // .style("top", (event.pageY - 10) + "px")
-                    // .style("display", "block")
-                    // .style("visibility", "visible")
+                // .on("mouseover", function (event, d) {
+                //     // マウスオーバー時の処理
+                //     // tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI)
+                //     // .style("left", (event.pageX + 10) + "px")
+                //     // .style("top", (event.pageY - 10) + "px")
+                //     // .style("display", "block")
+                //     // .style("visibility", "visible")
 
-                    if (d.properties.HVI == 0) {
-                        tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is not vulnerable to Urban Heat Island.")
-                            .style("left", (event.pageX + 10) + "px")
-                            .style("top", (event.pageY - 10) + "px")
-                            .style("display", "block")
-                            .style("visibility", "visible")
-                    } else if (d.properties.HVI == 1) {
-                        tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is slightly vulnerable to Urban Heat Island.")
-                            .style("left", (event.pageX + 10) + "px")
-                            .style("top", (event.pageY - 10) + "px")
-                            .style("display", "block")
-                            .style("visibility", "visible")
-                    } else if (d.properties.HVI == 2) {
-                        tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is moderately vulnerable to Urban Heat Island.")
-                            .style("left", (event.pageX + 10) + "px")
-                            .style("top", (event.pageY - 10) + "px")
-                            .style("display", "block")
-                            .style("visibility", "visible")
-                    } else if (d.properties.HVI == 3) {
-                        tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is highly vulnerable to Urban Heat Island.")
-                            .style("left", (event.pageX + 10) + "px")
-                            .style("top", (event.pageY - 10) + "px")
-                            .style("display", "block")
-                            .style("visibility", "visible")
-                    } else if (d.properties.HVI == 4) {
-                        tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is extremely vulnerable to Urban Heat Island.")
-                            .style("left", (event.pageX + 10) + "px")
-                            .style("top", (event.pageY - 10) + "px")
-                            .style("display", "block")
-                            .style("visibility", "visible")
-                    } else if (d.properties.HVI == 5) {
-                        tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is severely vulnerable to Urban Heat Island.")
-                            .style("left", (event.pageX + 10) + "px")
-                            .style("top", (event.pageY - 10) + "px")
-                            .style("display", "block")
-                            .style("visibility", "visible")
-                    } else { return "blue" }
+                //     if (d.properties.HVI == 0) {
+                //         tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is not vulnerable to Urban Heat Island.")
+                //             .style("left", (event.pageX + 10) + "px")
+                //             .style("top", (event.pageY - 10) + "px")
+                //             .style("display", "block")
+                //             .style("visibility", "visible")
+                //     } else if (d.properties.HVI == 1) {
+                //         tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is slightly vulnerable to Urban Heat Island.")
+                //             .style("left", (event.pageX + 10) + "px")
+                //             .style("top", (event.pageY - 10) + "px")
+                //             .style("display", "block")
+                //             .style("visibility", "visible")
+                //     } else if (d.properties.HVI == 2) {
+                //         tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is moderately vulnerable to Urban Heat Island.")
+                //             .style("left", (event.pageX + 10) + "px")
+                //             .style("top", (event.pageY - 10) + "px")
+                //             .style("display", "block")
+                //             .style("visibility", "visible")
+                //     } else if (d.properties.HVI == 3) {
+                //         tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is highly vulnerable to Urban Heat Island.")
+                //             .style("left", (event.pageX + 10) + "px")
+                //             .style("top", (event.pageY - 10) + "px")
+                //             .style("display", "block")
+                //             .style("visibility", "visible")
+                //     } else if (d.properties.HVI == 4) {
+                //         tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is extremely vulnerable to Urban Heat Island.")
+                //             .style("left", (event.pageX + 10) + "px")
+                //             .style("top", (event.pageY - 10) + "px")
+                //             .style("display", "block")
+                //             .style("visibility", "visible")
+                //     } else if (d.properties.HVI == 5) {
+                //         tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is severely vulnerable to Urban Heat Island.")
+                //             .style("left", (event.pageX + 10) + "px")
+                //             .style("top", (event.pageY - 10) + "px")
+                //             .style("display", "block")
+                //             .style("visibility", "visible")
+                //     } else { return "blue" }
 
 
-                })
+                // })
                 .on("mouseout", function (d) {
-                    console.log(d)
+
                     // マウスアウト時の処理
                     tooltip.style("display", "none");
                 })
-                .on("click", function (event, d) {
-                    console.log(event, d)
+                // .on("click", function (event, d) {
+                //     console.log(event, d)
 
-                    d3.select(this).style("fill", "orange");
-                })
+                //     d3.select(this).style("fill", "orange");
+                // })
                 .on("click", function (event, d) {
                     console.log(event, d)
                     // クリックされたパスの色を変更
@@ -242,15 +242,113 @@ export default {
                             }
                         });
                     }
+
+                    if (selectedPath && selectedPath.node() === this) {
+                        // 以前に選択されたパスがクリックされた場合、選択を解除
+                        selectedPath = null;
+                        return;
+                    }
                     // 新しいパスの色を変更
-                    d3.select(this).style("fill", "pink");
+                    d3.select(this).style("fill", "grey");
+                    
                     // 選択されたパスを更新
                     selectedPath = d3.select(this);
+                    
+                    // 選択されたパスのデータを取得
+                    const selectedData = d.properties;
+
+                    // tooltipの表示内容を作成
+                    const tooltipContent = "Suburb: " + selectedData.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + selectedData.HVI;
+
+                    if (selectedData.HVI == 0) {
+                        tooltipContent += "<br><br>This suburb is not vulnerable to Urban Heat Island.";
+                    } else if (selectedData.HVI == 1) {
+                        tooltipContent += "<br><br>This suburb is slightly vulnerable to Urban Heat Island.";
+                    } else if (selectedData.HVI == 2) {
+                        tooltipContent += "<br><br>This suburb is moderately vulnerable to Urban Heat Island.";
+                    } else if (selectedData.HVI == 3) {
+                        tooltipContent += "<br><br>This suburb is highly vulnerable to Urban Heat Island.";
+                    } else if (selectedData.HVI == 4) {
+                        tooltipContent += "<br><br>This suburb is extremely vulnerable to Urban Heat Island.";
+                    } else if (selectedData.HVI == 5) {
+                        tooltipContent += "<br><br>This suburb is severely vulnerable to Urban Heat Island.";
+                    } else {
+                        tooltipContent += "<br><br>Unknown vulnerability level.";
+                    }
+
+                                // tooltipを表示
+                    tooltip.html(tooltipContent)
+                        // .style("left", (event.pageX + 10) + "px")
+                        // .style("top", (event.pageY - 10) + "px")
+                        .style("display", "block")
+                        .style("visibility", "visible");
+
+                    const c_path = this    
 
 
 
 
-                });
+
+
+                })
+                .on("mouseover", function(event, d) {
+                        // マウスオーバー時の処理
+                // tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI)
+                // .style("left", (event.pageX + 10) + "px")
+                // .style("top", (event.pageY - 10) + "px")
+                // .style("display", "block")
+                // .style("visibility", "visible")
+
+                if (d.properties.HVI == 0 && !selectedPath) {
+                    tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is not vulnerable to Urban Heat Island.")
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 10) + "px")
+                    .style("display", "block")
+                    .style("visibility", "visible")
+                } else if (d.properties.HVI == 1 && !selectedPath) {
+                    tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is slightly vulnerable to Urban Heat Island.")
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 10) + "px")
+                    .style("display", "block")
+                    .style("visibility", "visible")
+                } else if (d.properties.HVI == 2 && !selectedPath) {
+                    tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" +  "This suburb is moderately vulnerable to Urban Heat Island.")
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 10) + "px")
+                    .style("display", "block")
+                    .style("visibility", "visible")
+                } else if (d.properties.HVI == 3 && !selectedPath) {
+                    tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is highly vulnerable to Urban Heat Island.")
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 10) + "px")
+                    .style("display", "block")
+                    .style("visibility", "visible")
+                } else if (d.properties.HVI == 4 && !selectedPath) {
+                    tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is extremely vulnerable to Urban Heat Island.")
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 10) + "px")
+                    .style("display", "block")
+                    .style("visibility", "visible")
+                } else if (d.properties.HVI == 5 && !selectedPath) { 
+                    tooltip.html("Suburb: " + d.properties.SA2_NAME16 + "<br>" + "Urban Heat Island Vulnerability Index: " + d.properties.HVI + "<br>" + "<br>" + "This suburb is severely vulnerable to Urban Heat Island.")
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 10) + "px")
+                    .style("display", "block")
+                    .style("visibility", "visible")
+                } else {return "blue"}
+
+
+                })
+                .on("mouseout", function(event, d) {
+                if (selectedPath) {
+                    tooltip.html(tooltipContent)
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 10) + "px")
+                    .style("display", "block")
+                    .style("visibility", "visible");
+                }
+
+    });
 
             let isClicking = false; // マウスがクリックされているかどうかの状態
 
